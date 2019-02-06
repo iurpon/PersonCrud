270308bb3f6e0bf6291abda06b35396a5d57b1c2
trandefil <trandafir.iura@gmail.com>
2019-02-06 18:26:28 +0300

    delete src

--- src/main/java/ru/trandefil/tm/util/UUIDUtil.java
+++
@@ -1,9 +0,0 @@
-package ru.trandefil.tm.util;
-
-import java.util.UUID;
-
-public class UUIDUtil {
-    public static String getUniqueString(){
-        return UUID.randomUUID().toString();
-    }
-}

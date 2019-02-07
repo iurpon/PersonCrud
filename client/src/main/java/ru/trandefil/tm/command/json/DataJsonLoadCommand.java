1d2b7e40976cf99ed04bde3cf7e98fb2eed38b1d
trandefil <trandafir.iura@gmail.com>
2019-02-07 17:48:51 +0300

    first ws test.4

--- client/src/main/java/ru/trandefil/tm/command/json/DataJsonLoadCommand.java
+++ client/src/main/java/ru/trandefil/tm/command/json/DataJsonLoadCommand.java
@@ -1,14 +1,6 @@
 package ru.trandefil.tm.command.json;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import ru.trandefil.tm.command.AbstractCommand;
-import ru.trandefil.tm.command.Domain;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-import static ru.trandefil.tm.util.FilterCollectionUtil.*;
 
 public class DataJsonLoadCommand extends AbstractCommand {
 
@@ -24,17 +16,7 @@ public class DataJsonLoadCommand extends AbstractCommand {
 
     @Override
     public void execute() {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            String xmlString = new String(Files.readAllBytes(Paths.get("data.json")));
-            Domain objectFactory = objectMapper.readValue(xmlString, Domain.class);
-            printProjectCollection(objectFactory.getProjectList(), getServiceLocator().getLoggedUser());
-            printUserCollection(objectFactory.getUserList(), getServiceLocator().getLoggedUser());
-            printTaskCollection(objectFactory.getTaskList(), getServiceLocator().getLoggedUser());
-        } catch (IOException e) {
-            System.out.println("is empty.");
-            e.printStackTrace();
-        }
+
     }
 
     @Override

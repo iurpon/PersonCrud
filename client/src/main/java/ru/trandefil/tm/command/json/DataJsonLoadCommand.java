3b481fa4d9daeeb503ede789621b0499a654f87f
trandefil <trandafir.iura@gmail.com>
2019-02-12 11:23:20 +0300

    generated

--- client/src/main/java/ru/trandefil/tm/command/json/DataJsonLoadCommand.java
+++ client/src/main/java/ru/trandefil/tm/command/json/DataJsonLoadCommand.java
@@ -1,7 +1,6 @@
 package ru.trandefil.tm.command.json;
 
 import ru.trandefil.tm.command.AbstractCommand;
-import ru.trandefil.tm.generated.Domain;
 import ru.trandefil.tm.generated.FileEndPoint;
 
 public class DataJsonLoadCommand extends AbstractCommand {
@@ -19,10 +18,10 @@ public class DataJsonLoadCommand extends AbstractCommand {
     @Override
     public void execute() {
         final FileEndPoint fileEndPoint = getServiceLocator().getFileEndPoint();
-        final Domain domain = fileEndPoint.loadJson();
+/*        final Domain domain = fileEndPoint.loadJson();
         domain.getProjectList().forEach(System.out::println);
         domain.getTaskList().forEach(System.out::println);
-        domain.getUserList().forEach(System.out::println);
+        domain.getUserList().forEach(System.out::println);*/
     }
 
     @Override
